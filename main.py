7fc30510644b15f7d8752e1b0ba67268240d60c5
FaustinoAndres <49380815+FaustinoAndres@users.noreply.github.com>
2022-04-25 01:05:45 -0400

    request response

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello world Flask"
+def index():
+    user_ip = request.remote_addr
 
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return f"Hello world Platzi, tu IP es {user_ip}"
 
 if __name__ == '__main__':
 
-   app.run()
+    app.run(debug=True)
